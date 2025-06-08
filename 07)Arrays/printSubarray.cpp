7495fd28b79cc5194ec1a30789fd1d32d2dd5351
Jatin5760 <jatinsharma14202003@gmail.com>
2025-06-08 12:09:20 +0530

    Commit

---
+++ 07)Arrays/printSubarray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {4,3,7,2};
+    int size = 4;
+
+
+    for(int i = 0; i<size; i++){
+        for(int j = i; j<size; j++){
+            for(int k = i; k<=j; k++){
+                cout<<arr[k]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
