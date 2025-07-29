27489759a2243604ad34faaa137d45fd32c0f9e2
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-29 14:21:37 +0530

    Commit

--- 24)LinkedList I/test.cpp
+++ 24)LinkedList I/test.cpp
@@ -1,10 +1,46 @@
+// This is Insertion code at the begining
+// #include<iostream>
+// using namespace std;
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+//     Node(int value){
+//         data = value;
+//         next = NULL;
+//     }
+// };
+// int main(){
+//     Node* Head;
+//     Head = NULL;
+//     int arr[] = {2,4,6,8,10};
+//     for(int i = 0; i< 5; i++){
+//         if(Head == NULL){
+//             Head = new Node(arr[i]);
+//         }
+//         else{
+//             Node* Temp;
+//             Temp = new Node(arr[i]);
+//             Temp->next = Head;
+//             Head = Temp;
+//         }
+//     }
+//     Node* Print = Head;
+//     while(Print != NULL){
+//         cout<< Print->data<<" ";
+//         Print = Print->next;
+//     }
+//     return 0;
+// }
+
+// Insertion at end
 #include<iostream>
 using namespace std;
 class Node{
+
     public:
     int data;
     Node* next;
-
 
     Node(int value){
         data = value;
@@ -15,24 +51,26 @@ int main(){
 
     Node* Head;
     Head = NULL;
+    Node* Tail;
+    Tail = NULL;
 
     int arr[] = {2,4,6,8,10};
 
-    for(int i = 0; i< 5; i++){
+    for(int i = 0; i < 5; i++){
         if(Head == NULL){
             Head = new Node(arr[i]);
+            Tail = Head;
         }
         else{
-            Node* Temp;
-            Temp = new Node(arr[i]);
-            Temp->next = Head;
-            Head = Temp;
+            Tail->next = new Node(arr[i]);
+            Tail = Tail->next;
         }
     }
-    Node* Print = Head;
-    while(Print != NULL){
-        cout<< Print->data<<" ";
-        Print = Print->next;
+
+    Node* Temp = Head;
+    while(Temp){
+        cout<< Temp->data<<" ";
+        Temp = Temp->next;
     }
     return 0;
 }
