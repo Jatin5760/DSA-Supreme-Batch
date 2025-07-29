67fea500179a8392c93d530da9236cd99bc04aae
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-29 22:48:46 +0530

    Commit

--- 24)LinkedList I/test.cpp
+++ 24)LinkedList I/test.cpp
@@ -34,43 +34,47 @@
 // }
 
 // Insertion at end
+// #include<iostream>
+// using namespace std;
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+//     Node(int value){
+//         data = value;
+//         next = NULL;
+//     }
+// };
+// int main(){
+//     Node* Head;
+//     Head = NULL;
+//     Node* Tail;
+//     Tail = NULL;
+//     int arr[] = {2,4,6,8,10};
+//     for(int i = 0; i < 5; i++){
+//         if(Head == NULL){
+//             Head = new Node(arr[i]);
+//             Tail = Head;
+//         }
+//         else{
+//             Tail->next = new Node(arr[i]);
+//             Tail = Tail->next;
+//         }
+//     }
+//     Node* Temp = Head;
+//     while(Temp){
+//         cout<< Temp->data<<" ";
+//         Temp = Temp->next;
+//     }
+//     return 0;
+// }
+
+// Create List using recurrsion
 #include<iostream>
 using namespace std;
-class Node{
 
-    public:
-    int data;
-    Node* next;
-
-    Node(int value){
-        data = value;
-        next = NULL;
-    }
-};
 int main(){
 
-    Node* Head;
-    Head = NULL;
-    Node* Tail;
-    Tail = NULL;
 
-    int arr[] = {2,4,6,8,10};
-
-    for(int i = 0; i < 5; i++){
-        if(Head == NULL){
-            Head = new Node(arr[i]);
-            Tail = Head;
-        }
-        else{
-            Tail->next = new Node(arr[i]);
-            Tail = Tail->next;
-        }
-    }
-
-    Node* Temp = Head;
-    while(Temp){
-        cout<< Temp->data<<" ";
-        Temp = Temp->next;
-    }
     return 0;
 }
