c02361572e23134b127feb9c1c22d52d08bd8531
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-29 13:25:39 +0530

    Commit

--- 24)LinkedList I/test.cpp
+++ 24)LinkedList I/test.cpp
@@ -1,45 +1,9 @@
 #include<iostream>
 using namespace std;
-class Node
-    {
-        public:
-        int data;
-        Node* next;
+class Node{
 
-        Node(int value)
-            {
-                data = value;
-                next = NULL;
-            }
-    };
-int main()
-    {
-        Node* Head, * Tail;
-        Head = NULL;
-        Tail = NULL;
+}
+int main(){
 
-        int arr[] = {2,4,6,8,10};
-
-        for(int i = 0; i<5; i++)
-            {
-                if(Head == NULL)
-                    {
-                        Head = new Node(arr[i]);
-                        Tail = Head;
-                    }
-                else
-                    {
-                        Tail->next = new Node(arr[i]);
-                        Tail = Tail->next;
-                    }
-            }
-
-        Node* Temp = Head;
-        while(Temp)
-            {
-                cout<<Temp->data<<" ";
-                Temp = Temp->next;
-            }
-
-        return 0;
-    }
+    return 0;
+}
