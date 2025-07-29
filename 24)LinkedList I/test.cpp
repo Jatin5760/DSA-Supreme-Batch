adbdda9a2342f5ecd13844b4a738d04e718032be
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-29 13:36:46 +0530

    Commit

--- 24)LinkedList I/test.cpp
+++ 24)LinkedList I/test.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 class Node{
+    public:
+    int data;
+    Node* next;
 
-}
+
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+};
 int main(){
+
+    Node* Head;
+    Head = new Node(5);
+
+    cout<< Head;
 
     return 0;
 }
