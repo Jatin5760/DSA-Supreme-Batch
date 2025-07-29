efceb389f69d2db26a08fbae8e248a1be57e1965
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-30 02:04:30 +0530

    Commit

--- 24)LinkedList I/test.cpp
+++ 24)LinkedList I/test.cpp
@@ -107,41 +107,59 @@
 // }
 
 // Create List using recurrsion by adding elements in start
+// #include<iostream>
+// using namespace std;
+// class Node{
+//     public:
+//     int data;       
+//     Node* next;
+//     Node(int value){
+//         data = value;
+//         next = NULL;
+//     }
+// };
+// Node* CreateLLUsingRecurr(int arr[], int size, int index, Node* prev){
+//     // Base Case
+//     if(index == size){
+//         return prev;
+//     }
+//     Node* Temp;
+//     Temp = new Node(arr[index]);
+//     Temp->next = prev;
+//     return CreateLLUsingRecurr(arr, size, index + 1, Temp);
+// };
+// int main(){
+//     Node* Head;
+//     Head = NULL;
+//     int arr[] = {2,4,6,8,10};
+//     int size = 5;
+//     int index = 0;
+//     Head = CreateLLUsingRecurr(arr, size, index, Head);
+//     Node* Temp = Head;
+//     while(Temp!=NULL){
+//         cout<<Temp->data<<" ";
+//         Temp = Temp->next;
+//     }
+//     return 0;
+// }
+
+// Insert at particular point
 #include<iostream>
 using namespace std;
 class Node{
     public:
-    int data;       
+    int data;
     Node* next;
     Node(int value){
         data = value;
         next = NULL;
     }
 };
-Node* CreateLLUsingRecurr(int arr[], int size, int index, Node* prev){
-    // Base Case
-    if(index == size){
-        return prev;
-    }
-    Node* Temp;
-    Temp = new Node(arr[index]);
-    Temp->next = prev;
-    return CreateLLUsingRecurr(arr, size, index + 1, Temp);
-};
 int main(){
     Node* Head;
     Head = NULL;
+
     int arr[] = {2,4,6,8,10};
-    int size = 5;
-    int index = 0;
-    Head = CreateLLUsingRecurr(arr, size, index, Head);
-    Node* Temp = Head;
-    while(Temp!=NULL){
-        cout<<Temp->data<<" ";
-        Temp = Temp->next;
-    }
+    
     return 0;
 }
-
-
-
