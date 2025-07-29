57cdb84f261ab5e3380fd5f73eed358041eba11c
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-29 23:53:50 +0530

    Commit

--- 24)LinkedList I/test.cpp
+++ 24)LinkedList I/test.cpp
@@ -70,11 +70,44 @@
 // }
 
 // Create List using recurrsion
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+//     Node(int value){
+//         data = value;
+//         next = NULL;
+//     }
+// };
+// Node* CreateLLUsingRecurr(int arr[], int size, int index){
+//     // Base Case
+//     if(index == size){
+//         return NULL;
+//     }
+//     Node* Temp;
+//     Temp = new Node(arr[index]);
+//     Temp->next = CreateLLUsingRecurr(arr, size, index+1);
+//     return Temp;
+// };
+// int main(){
+//     Node* Head;
+//     Head = NULL;
+//     int arr[] = {2,4,6,8,10};
+//     int size = 5;
+//     int index = 0;
+//     Head = CreateLLUsingRecurr(arr, size, index);
+//     Node* Temp = Head;
+//     while(Temp!=NULL){
+//         cout<<Temp->data<<" ";
+//         Temp = Temp->next;
+//     }
+//     return 0;
+// }
 
-int main(){
+// Create List using recurrsion by adding elements in start
 
 
-    return 0;
-}
+
+
