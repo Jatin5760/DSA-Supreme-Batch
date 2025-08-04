67fad23759dc1723efd309457489e7129f559a9c
Jatin5760 <jatinsharma14202003@gmail.com>
2025-08-04 12:20:32 +0530

    Commit

--- 25)LinkedList ||/Middle_of_LL.cpp
+++ 25)LinkedList ||/Middle_of_LL.cpp
@@ -1,4 +1,4 @@
-// Method 1: Traversal with count parameter
+ // Method 1: Traversal with count parameter
 
 class Solution {
 public:
@@ -24,4 +24,14 @@ public:
 
 
 // Method 2: Fast and slow pointer
-
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode * slow = head, *fast = head;
+//         while(fast!=NULL && fast->next!=NULL){
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+//         return slow;
+//     }
+// };
