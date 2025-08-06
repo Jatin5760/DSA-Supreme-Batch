954ffb4907e343b7494f1b2e327838c7baa17be9
Jatin5760 <jatinsharma14202003@gmail.com>
2025-08-06 16:00:05 +0530

    Commit

--- 26)DoublyLinkedList/test.cpp
+++ 26)DoublyLinkedList/test.cpp
@@ -19,13 +19,22 @@ Node* CreateDLL(int arr[], int index, int size, Node* back){
     if(index == size){
         return NULL;
     }
-    Node* temp = new Node(arr[index])
+    Node* temp = new Node(arr[index]);
     temp->prev = back;
     temp->next = CreateDLL(arr, index + 1, size, temp); 
 
     return temp; 
 }
 int main(){
+    int arr[] = {1,2,3,4,5,500};
 
-    
+    Node* head;
+    head = CreateDLL(arr,0,6,NULL);
+
+    Node* trev = head;
+    while(trev!=NULL){
+        cout<<trev->data<<" ";
+        trev = trev->next;
+    }
+    return 0;
 }
