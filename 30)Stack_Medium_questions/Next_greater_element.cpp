4ea17734358c3b4b877075b969a615c33f427eae
Jatin5760 <jatinsharma14202003@gmail.com>
2025-08-18 21:54:23 +0530

    Commit

--- 30)Stack_Medium_questions/Next_greater_element.cpp
+++ 30)Stack_Medium_questions/Next_greater_element.cpp
@@ -21,14 +21,9 @@ Stack<int> st;
 Vector<int> arr(n, -1);
 
 for (int i = 0; i < n; i++){
-    // if (st.empty()){
-    //     st.push(i);
-    // }
-    else{
         while(!st.empty() && arr[st.top()] < arr[i]){
             ans[st.top()] = arr[i];
             st.pop();
         }
         st.push(i);
     }
-}
