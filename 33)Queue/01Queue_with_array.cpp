e6a511ac67001efc5cf76f915cc355f6f72b5180
Jatin5760 <jatinsharma14202003@gmail.com>
2025-08-30 11:32:36 +0530

    Commit

---
+++ 33)Queue/01Queue_with_array.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+// Queue with arrays
+class Queue
+{
+    int *arr;
+    int front;
+    int rear;
+    int size;
+
+public:
+    // Constructor
+    Queue(int n)
+    {
+        arr = new int[n];
+        front = -1;
+        rear = -1;
+        size = n;
+    }
+
+    bool isEmpty()
+    {
+        return front == -1;
+    }
+    bool isFull()
+    {
+        return rear == size - 1;
+    }
+
+    void push(int x)
+    {
+        if (isFull())
+        {
+            cout << "Queue is overflow";
+            return;
+        }
+        else if (isEmpty())
+        {
+            cout<<"Pushed "<< x <<" into the Queue"<<endl;
+            front = rear = 0;
+            arr[0] = x;
+            return;
+        }
+        else
+        {
+            rear = rear + 1;
+            arr[rear] = x;
+            cout<<"Pushed "<< x <<" into the Queue"<<endl;
+        }
+    }
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is underflow";
+            return;
+        }
+        else
+        {
+            if (front == rear)
+            {
+                cout<<"Popped "<< arr[front] <<" from the Queue"<<endl;
+                front = -1;
+                rear = -1;
+            }
+            else
+            {
+                cout<<"Popped "<< arr[front] <<" from the Queue"<<endl;
+                front = front + 1;
+            }
+        }
+    }
+    
+    int start(){
+        if(isEmpty()){
+            cout<<"Queue is Empty";
+            return -1;
+        }
+        else{
+            return arr[front];
+        }
+    }
+};
+
+int main()
+{
+    Queue q(5);
+    q.push(5);
+    q.push(15);
+    q.push(51);
+
+    
+}
