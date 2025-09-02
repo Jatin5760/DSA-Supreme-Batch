617cd099c5e53be403bdd015b67ec901d3699b0d
Jatin5760 <jatinsharma14202003@gmail.com>
2025-09-02 22:21:48 +0530

    Commit

---
+++ 34)Trees/02_Tree_With_Recurrsion.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left, * right;
+
+    Node(int value){
+        data = value;
+        left = right = NULL;
+    }
+};
+
+Node* binaryTree(){
+    int x;
+    cin>>x;
+
+    // Base Condition
+    if(x == -1){
+        return NULL;
+    }
+
+    Node* temp = new Node(x);
+    // Left child creation
+    cout<<"Enter the left child of "<<x<<"  : ";
+    temp->left = binaryTree();
+    // Right child creation
+    cout<<"Enter the right child of "<<x<<" : ";
+    temp->right = binaryTree();
+
+    return temp;
+}
+int main(){
+    
+    cout<<"Enter the root Node: ";
+    Node* root;
+    root = binaryTree();
+    return 0;
+}
