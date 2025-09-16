ca12ace6b320f6ecd1d4078ab366a201073fe7cc
Jatin5760 <jatinsharma14202003@gmail.com>
2025-09-16 12:14:14 +0530

    commit

--- 35)STL/list.cpp
+++ 35)STL/list.cpp
@@ -11,13 +11,22 @@ int main(){
     l1.push_back(50);
     l1.push_back(60);
     l1.push_back(70);
+    l1.push_front(15);
 
-    cout<<l1.front()<<" "<<l1.back()<<endl;
+    //cout<<l1.front()<<" "<<l1.back()<<endl;
 
     l1.pop_back();
     l1.pop_front();
 
-    cout<<l1.front()<<" "<<l1.back()<<endl;
+    //cout<<l1.front()<<" "<<l1.back()<<endl;
+
+    for(auto it: l1){
+        cout<<it<<endl;
+    }
+
+    for(auto it = l1.begin(); it!= l1.end(); it++){
+        cout<<*it<<" ";
+    }
     
 
     return 0;
