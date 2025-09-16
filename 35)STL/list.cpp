9d0e9d9f763b09984e46688faf67b03aa0b2ecbe
Jatin5760 <jatinsharma14202003@gmail.com>
2025-09-16 12:22:02 +0530

    commit

--- 35)STL/list.cpp
+++ 35)STL/list.cpp
@@ -8,6 +8,8 @@ int main(){
 
     // 15 20 30 40 50 60 70 
     // l.end() 70 ke baad wale ko print karega that is why for loop mai it!=l.end() likha hai hamne.
+    // l.rbegin() - 70 ko point karega.
+    // l.rend() - 15 ke phele wale ko point karega
     l1.push_back(20);
     l1.push_back(30);
     l1.push_back(40);
@@ -28,6 +30,11 @@ int main(){
     }
 
     for(auto it = l1.begin(); it!= l1.end(); it++){
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+
+    for(auto it = l1.rbegin(); it!=l1.rend(); it++){
         cout<<*it<<" ";
     }
     
