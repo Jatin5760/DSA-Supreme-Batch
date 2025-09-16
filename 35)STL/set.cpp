e13c11346252fd6cfe15eda6c15ebbf5180cec89
Jatin5760 <jatinsharma14202003@gmail.com>
2025-09-16 12:32:58 +0530

    commit

--- 35)STL/set.cpp
+++ 35)STL/set.cpp
@@ -6,12 +6,24 @@ int main(){
 
     // SET - Stores only unique value
     // Set ki implementation AVL tree se hui hai kyonki waha par bhi sare operations mai O(log n).
+    // Ye Red-Black tree se bhi implement kar sakte hai AVL se thoda sa advantage hai kyonki kam roations lagte hai.
 
     // Store values in sorted order.
 
     // Serching - O(log n)
     // Insert - O(log n)
     // Delete - O(log n)
+
+
+    set<int>s;
+    s.insert(20);
+    s.insert(30);
+    s.insert(20); // will not be inserted
+    s.insert(40);
+
+    for(auto it: s){
+        cout<<it<<" ";
+    }
 
     return 0;
 }
