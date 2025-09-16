0ace42658ef0edfe96b0ca78bad0bdb31accc57c
Jatin5760 <jatinsharma14202003@gmail.com>
2025-09-16 12:40:32 +0530

    commit

--- 35)STL/set.cpp
+++ 35)STL/set.cpp
@@ -25,5 +25,44 @@ int main(){
         cout<<it<<" ";
     }
 
+    cout<<endl;
+
+    for(auto it = s.rbegin(); it!=s.rend(); it++){
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+
+
+    // We can sort set in decending order using greater<type>s;
+    set<int, greater<int> >s1;
+    s1.insert(10);
+    s1.insert(20);
+    s1.insert(30);
+    s1.insert(40);
+    s1.insert(50);
+    s1.insert(60);
+
+    for(auto it: s1){
+        cout<<it<<" ";
+    }
+
+    cout<<endl;
+
+    // find
+    if(s.find(20) !=s.end()){
+        cout<<"Present"<<endl;
+    }
+    else{
+        cout<<"Absent"<<endl;
+    }
+
+    // count
+    if(s.count(200)){
+        cout<<"Present\n";
+    }
+    else{
+        cout<<"Absent\n";
+    }
+
     return 0;
 }
