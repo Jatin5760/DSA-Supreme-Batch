e05c3c6f8a9b0f998853a028e38bf8f48bdab4eb
Jatin Sharma <142175713+Jatin5760@users.noreply.github.com>
2026-01-05 12:28:25 +0530

    Time: 0 ms (100%), Space: 27.2 MB (50.73%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
+    void rev(vector<char>& s, int l , int r){
+        if(l>=r){
+            return;
+        }
+        swap(s[l], s[r]);
+        rev(s, l + 1, r - 1);
+    }
+
     void reverseString(vector<char>& s) {
+    
+    // Method - 1 (In place) - Two Pointer 
+        
+        /* 
         int start = 0;
         int end = s.size() - 1;
         while(start <= end){
@@ -8,5 +20,10 @@ public:
             start++;
             end--;
         }
+        */
+    
+    // Method - 2 Recurrsion
+        rev(s, 0, s.size() - 1);
+
     }
 };
