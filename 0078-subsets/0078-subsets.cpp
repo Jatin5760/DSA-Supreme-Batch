1f801f0ee8f066ea7af5adf2a8b2fe6b11efebc7
Jatin Sharma <142175713+Jatin5760@users.noreply.github.com>
2026-01-09 12:36:30 +0530

    Time: 3 ms (30.27%), Space: 15.8 MB (9.73%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     void setsub(vector<int>& nums, int index, int n, vector<int>part, vector<vector<int>>& ans){
         // Base case
-        if(index == n){
-            ans.push_back(part);
-            return;
-        }
-        part.push_back(nums[index]);
-        setsub(nums, index+1, n, part, ans);
-        part.pop_back();
-        setsub(nums, index + 1, n, part, ans);
+       if(index == n){
+        ans.push_back(part);
+        return;
+       }
+
+       part.push_back(nums[index]);
+       setsub(nums, index + 1, n, part, ans);
+       part.pop_back();
+       setsub(nums, index + 1, n, part, ans);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int> >ans;
-        vector<int>part;
-
-        setsub(nums, 0, n, part, ans);
-        return ans;
+      vector<int>part;
+      vector<vector<int>>ans;
+      setsub(nums, 0, nums.size(), part, ans);
+      return ans;
     }
 };
