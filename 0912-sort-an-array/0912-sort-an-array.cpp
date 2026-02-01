11448750c409103a3398fcd407eb1396d50e722f
Jatin Sharma <142175713+Jatin5760@users.noreply.github.com>
2026-02-01 10:04:06 +0530

    Time: 2228 ms (5.01%), Space: 71 MB (83.45%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*  Merge Sort
     void merge(vector<int>& nums, int s, int e, int mid){
         vector<int> output;
         int low = s;
@@ -40,10 +41,43 @@ public:
         // sorting 
         merge(nums, s, e, mid);
     }
+    */
+
+    // Quick Sort
+    void quickSort(vector<int>& nums, int start, int end){
+        
+        if(start >= end) return;
+
+        int i = start - 1;
+        int j = start;
+
+        int r = start + rand() % (end - start + 1);
+        swap(nums[r], nums[end]);
+
+        int pivot = end;
+
+        
+
+        while(j < pivot){
+            if(nums[j] < nums[pivot]){
+                i++;
+                swap(nums[i], nums[j]);
+            }
+            j++;
+        }
+        i++;
+        swap(nums[i], nums[pivot]);
+        quickSort(nums, start, i - 1);
+        quickSort(nums, i+1, end);
+    }
     vector<int> sortArray(vector<int>& nums) {
-        int s = 0;
-        int e = nums.size()-1;
-        mergeSort(nums, s, e);
+        // int s = 0;
+        // int e = nums.size()-1;
+        // mergeSort(nums, s, e);
+        
+        int start = 0;
+        int end = nums.size() - 1;
+        quickSort(nums, start , end);
 
         return nums;
     }
