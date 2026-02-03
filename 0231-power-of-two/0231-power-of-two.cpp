744598cbd6f5da6632aa76d750e051e25c1a01dd
Jatin Sharma <142175713+Jatin5760@users.noreply.github.com>
2026-02-03 12:46:43 +0530

    Time: 0 ms (100%), Space: 7.9 MB (13%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool power(int n){
+        if(n <= 0){
+            return false;
+        }
+        if(n == 1){
+            return true;
+        }
+        if(n%2!=0)return false;
+       return power(n/2);
+    }
+    bool isPowerOfTwo(int n) {
+        if(n == 1){
+            return true;
+        }
+        return power(n);
+    }
+};
