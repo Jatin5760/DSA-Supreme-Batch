b3e3c1d3c25a3d7ea335f98d1878c9b9e84ae1a6
Jatin Sharma <142175713+Jatin5760@users.noreply.github.com>
2026-02-12 17:39:42 +0530

    Time: 0 ms (100%), Space: 10.5 MB (62%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
-    int solve(vector<int>&nums, int index, vector<int>&dp){
-        if(index >= nums.size()){
-            return 0;
+// Tabulation
+    int solveTabulation(vector<int>&nums, int index, vector<int>&dp){
+        int n = nums.size();
+        dp[n] = 0;
+        for(int i = n - 1; i >= 0; i--){
+            int temp = 0;
+            if(i+2 <= n){
+                temp = dp[i+2];
+            }
+            int include = nums[i] + temp;
+            int exclude = 0 + dp[i + 1];
+            dp[i] = max(include, exclude);
         }
-        if(dp[index] != -1){
-            return dp[index];
-        }
-        int include = nums[index] + solve(nums, index + 2, dp);
-        int exclude = 0 + solve(nums, index + 1, dp);
-        int ans = max(include, exclude);
-        dp[index] = ans;
-        return ans;
+        return dp[0];
     }
+
+    // This is memoization
+
+    // int solve(vector<int>&nums, int index, vector<int>&dp){
+    //     if(index >= nums.size()){
+    //         return 0;
+    //     }
+    //     if(dp[index] != -1){
+    //         return dp[index];
+    //     }
+    //     int include = nums[index] + solve(nums, index + 2, dp);
+    //     int exclude = 0 + solve(nums, index + 1, dp);
+    //     int ans = max(include, exclude);
+    //     dp[index] = ans;
+    //     return ans;
+    // }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int>dp(n+1, -1);
-        int ans = solve(nums, 0, dp);
+        int ans = solveTabulation(nums, 0, dp);
         return ans;
     }
 };
