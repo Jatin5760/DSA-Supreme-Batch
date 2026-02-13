8e111dfb101222805defdcae03c7c96c3cd09823
Jatin Sharma <142175713+Jatin5760@users.noreply.github.com>
2026-02-14 00:25:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,26 +1,31 @@
 class Solution {
   public:
-    int solve(vector<int>& height, vector<int>& dp, int n){
-        if(n == 0){
+    int solve(vector<int>& height, int index, vector<int>&dp){
+        //  Base Case
+        if(index >= height.size() - 1){
             return 0;
         }
-        if(dp[n] != -1){
-            return dp[n];
-        }
         
-        int left = solve(height, dp, n - 1) + abs(height[n] - height[n-1]);
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        int left = solve(height, index + 1, dp) + abs(height[index+1] - height[index]);
         int right = INT_MAX;
         
-        if(n > 1){
-            right = solve(height, dp, n - 2) + abs(height[n] - height[n-2]);
+        if((index + 2) < height.size()){
+            right = solve(height, index + 2, dp) + abs(height[index + 2] - height[index]);
         }
         
-        return dp[n] = min(left, right);
+        int ans = min(left, right);
+        dp[index] = ans;
+        return dp[index];
     }
     int minCost(vector<int>& height) {
         // Code here
         int n = height.size();
-        vector<int>dp(n+1, -1);
-        return solve(height, dp, n-1);
+        int index = 0;
+        vector<int>dp(n + 1, -1);
+        int ans = solve(height, index, dp);
+        return ans;
     }
 };
