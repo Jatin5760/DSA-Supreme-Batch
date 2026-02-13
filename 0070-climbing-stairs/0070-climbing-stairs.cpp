b1aba108f714d114bbc848c2b7277eb7b449ba5b
Jatin Sharma <142175713+Jatin5760@users.noreply.github.com>
2026-02-13 13:46:32 +0530

    Time: 0 ms (100%), Space: 8.5 MB (34.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,20 +3,26 @@ public:
     int solve(int n, vector<int>& dp){
         //  Base Case
 
-        if(n == 0){
-            return 1;
-        }
+        // if(n == 0){
+        //     return 1;
+        // }
 
-        if(n == 1){
-            return 1;
-        }
+        // if(n == 1){
+        //     return 1;
+        // }
 
-        if(dp[n] != -1){
-            return dp[n];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // if(dp[n] != -1){
+        //     return dp[n];
+        // }
+
+        for(int i = 2; i <= n; i++){
+            int ans = dp[i - 1] + dp[i-2];
+            dp[i] = ans;
+            
         }
-        
-        int ans = solve(n - 1, dp) + solve(n-2, dp);
-        dp[n] = ans;
         return dp[n];
     }
     int climbStairs(int n) {
