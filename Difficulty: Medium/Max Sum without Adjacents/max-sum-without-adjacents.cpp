a175efac9a14e07291131aa402a7498a8778e173
Jatin Sharma <142175713+Jatin5760@users.noreply.github.com>
2026-02-14 19:54:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
+++ Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
@@ -1,25 +1,26 @@
 // User function template for C++
 class Solution {
   public:
-    // calculate the maximum sum with out adjacent
-    int solve(int index,vector<int>& arr, vector<int>& dp){
-        if(index == 0){
-            arr[index];
-        }
-        if(index < 0){
+    int solve(vector<int>& arr, int index, vector<int>&dp){
+        // Base Case
+        if(index >= arr.size()){
             return 0;
         }
+        if(dp[index] != -1){
+            return dp[index];
+        }
         
-        if(dp[index] != -1)return dp[index];
-        int pick = arr[index]+solve(index-2, arr, dp);
-        int notpick = 0 + solve(index-1, arr,dp);
-        
-        return dp[index] = max(pick, notpick);
+        int pick = arr[index] + solve(arr, index + 2, dp);
+        int notpick = 0 + solve(arr, index + 1, dp);
+        return dp[index] =  max(pick, notpick);
     }
+    // calculate the maximum sum with out adjacent
     int findMaxSum(vector<int>& arr) {
         // code here
-        vector<int>dp(arr.size()+1, -1);
-        return solve(arr.size()-1, arr,dp);
-        
+        int index = 0;
+        int n = arr.size();
+        vector<int>dp(n + 1, -1);
+        int ans = solve(arr, index, dp);
+        return ans;
     }
 };
