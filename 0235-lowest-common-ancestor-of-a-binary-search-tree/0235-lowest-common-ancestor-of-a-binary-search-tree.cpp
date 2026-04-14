c6676fd79ebbaca0a6077819acbe25e2f7a36a0c
Jatin Sharma <142175713+Jatin5760@users.noreply.github.com>
2026-04-14 18:16:54 +0530

    Time: 22 ms (65.49%), Space: 23.5 MB (34.24%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,27 +11,44 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // if(!root) return NULL;
+
+        // if(root->val == p->val){
+        //     return p;
+        // }
+
+        // if(root->val == q->val){
+        //     return q;
+        // }
+
+        // TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        // TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        // if(!left && !right){
+        //     return NULL;
+        // }
+        // else if(left && !right){
+        //     return left;
+        // } 
+        // else if(!left && right){
+        //     return right;
+        // }
+        // return root;
+
+
+        // Approach 2 (log n)
         if(!root) return NULL;
-
-        if(root->val == p->val){
-            return p;
+        if(p->val < root->val && q->val < root->val){
+            TreeNode* left = lowestCommonAncestor(root->left, p, q);
+            if(left != NULL){
+                return left;
+            }
         }
-
-        if(root->val == q->val){
-            return q;
-        }
-
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-
-        if(!left && !right){
-            return NULL;
-        }
-        else if(left && !right){
-            return left;
-        } 
-        else if(!left && right){
-            return right;
+        if(p->val > root->val && q->val > root->val){
+            TreeNode* right = lowestCommonAncestor(root->right, p, q);
+            if(right != NULL){
+                return right;
+            }
         }
         return root;
     }
