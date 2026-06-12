a0a4a84eb1e3fa7b03162382373dcbec013b074a
Jatin Sharma <142175713+Jatin5760@users.noreply.github.com>
2026-06-12 22:28:21 +0530

    Time: 7 ms (45.82%), Space: 20.1 MB (24.87%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
+        int N = numCourses;
+        vector<int>inDegree(N, 0);
+        vector<vector<int>>adjList(N);
+        queue<int>q;
+        int count = 0;
+        for(auto edge: prerequisites){
+            int u = edge[0];
+            int v = edge[1];
+            adjList[u].push_back(v);
+            inDegree[v]++;
+        }
+        for(int i = 0; i < N; i++){
+            if(inDegree[i] == 0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int frontNode = q.front();
+            q.pop();
+            count++;
+            for(auto nbr: adjList[frontNode]){
+                inDegree[nbr]--;
+                if(inDegree[nbr] == 0){
+                    q.push(nbr);
+                }
+            }
+        }
+        if(count!=N)return false;
+        return true;
     }
 };
